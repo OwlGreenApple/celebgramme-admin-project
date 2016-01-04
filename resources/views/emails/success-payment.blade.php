58b74cbfbe2361d690e96d998bfdd92f530a065c
rizky redjo <it2.axiapro@gmail.com>
2016-01-04 16:15:35 +0700

    fixing email confirm payment

--- resources/views/emails/success-payment.blade.php
+++ resources/views/emails/success-payment.blade.php
@@ -2,6 +2,6 @@ Terima kasih anda telah melakukan pembayaran via {{$order_type}} <br>
 berikut adalah invoice anda <br>
 No Invoice : {{$no_invoice}}<br>
 Paket : {{$package->package_name}} <br>
-Total : {{$invoice->total}} <br>
+Total : Rp. {{number_format($invoice->total,0,'','.')}} <br>
 Salam hangat Celebgramme.com
 
