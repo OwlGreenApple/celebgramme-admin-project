0bb490b8cb85b5b43462f1a67c194e4b0654dfbc
rizky redjo <it2.axiapro@gmail.com>
2016-07-26 13:40:38 +0700

    fixing file yang ke hapus

--- resources/views/admin/setting-automation/index.blade.php
+++ resources/views/admin/setting-automation/index.blade.php
@@ -74,6 +74,27 @@
   </div>
 
 
+  <div class="modal fade" id="myModalLog" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">History Logs</h4>
+        </div>
+        <div class="modal-body">
+					<div class="form-group form-group-sm row">
+						<p id="p-logs" style="margin-left:10px;"></p>
+					</div>  
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+      
+    </div>
+  </div>
+	
+	
   <div class="modal fade" id="myModalDaily" role="dialog">
     <div class="modal-dialog">
       <!-- Modal content-->
