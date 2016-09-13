d838bca631e2fdd7528e49969c4302a8136b76a5
rizky redjo <it2.axiapro@gmail.com>
2016-09-13 14:07:31 +0700

    checking daily ditambah jadi 21 baris

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -476,7 +476,7 @@ class SettingController extends Controller {
 		}
 
 		$dt = Carbon::now()->setTimezone('Asia/Jakarta');		
-		for ($i=1;$i<=7;$i++) {
+		for ($i=1;$i<=21;$i++) {
 			$logs .= "<tr>";
 			$logs .= "<td>".$dt->toDateString()."</td>";
 			
