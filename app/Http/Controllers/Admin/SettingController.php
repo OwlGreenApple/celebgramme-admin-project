e907a40f8d98303cb5d5efd2717b5c96cd1c9b73
rizky redjo <it2.axiapro@gmail.com>
2016-10-28 16:08:30 +0700

    fixing

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -660,12 +660,10 @@ class SettingController extends Controller {
 					
     foreach ($settings as $setting) {					
 			if ($setting->server_automation == "A1(automation-1)") {
-				// $server = "http://192.186.146.248/";
-				continue;
+				$server = "http://192.186.146.248/";
 			}
 			if ($setting->server_automation == "A2(automation-2)") {
-				// $server = "http://192.186.146.246/";
-				continue;
+				$server = "http://192.186.146.246/";
 			}
 			if ($setting->server_automation == "A3(automation-3)") {
 				$server = "http://188.210.215.104/";
