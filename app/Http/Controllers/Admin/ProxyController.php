7975817947351248a08eb29436c3a27ca8bcfc8b
rizky redjo <it2.axiapro@gmail.com>
2017-08-06 22:25:45 +0700

    fixing

--- app/Http/Controllers/Admin/ProxyController.php
+++ app/Http/Controllers/Admin/ProxyController.php
@@ -10,7 +10,7 @@ use Celebgramme\Models\Proxies;
 use Celebgramme\Models\SettingHelper; 
 use Celebgramme\Models\ViewProxyUses;
 
-use View,Auth,Request,DB,Carbon,Excel, Mail, Validator;
+use View,Auth,Request,DB,Carbon,Excel, Mail, Validator, Input;
 
 class ProxyController extends Controller {
 
