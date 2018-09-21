289f5913536f236e937c8c1234123cb2aed5a3d4
PuspitaN <puspita.celebgramme@gmail.com>
2018-09-21 11:46:47 +0700

    price chart

--- resources/views/admin/price-chart/index.blade.php
+++ resources/views/admin/price-chart/index.blade.php
@@ -134,6 +134,16 @@
 
           chart.render();
 
+          function toggleDataSeries(e){
+            if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+              e.dataSeries.visible = false;
+            }
+            else{
+              e.dataSeries.visible = true;
+            }
+            chart.render();
+          }
+          
           $("#div-loading").hide();
         }
       });
